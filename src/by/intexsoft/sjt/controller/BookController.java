fdfa3e87b65c1cd7271473f8fcb7a43895bb2f06
eugeneJadeszko <zhenia04.94@mail.ru>
2017-08-25 13:46:53 +0300

    add logging

--- src/by/intexsoft/sjt/controller/BookController.java
+++ src/by/intexsoft/sjt/controller/BookController.java
@@ -2,6 +2,7 @@ package by.intexsoft.sjt.controller;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,40 +13,59 @@ import org.springframework.web.bind.annotation.RestController;
 import by.intexsoft.sjt.entity.BoardEntity;
 import by.intexsoft.sjt.entity.BookEntity;
 import by.intexsoft.sjt.service.BookService;
+import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
-	
+	private static Logger logger = (Logger) LoggerFactory.getLogger(BookController.class.getName());
+
 	@Autowired
 	BookService bookService;
 
-	@RequestMapping("/add/{tittle}/{author}")
-	public ResponseEntity<?> add(@PathVariable("tittle") String tittle, @PathVariable("author") String author) {
+	@RequestMapping("/add/{title}/{author}")
+	public ResponseEntity<?> add(@PathVariable("title") String title, @PathVariable("author") String author) {
+		logger.info("Creation of a new book with the name: " + title + " and author: " + author);
 		BookEntity book = new BookEntity();
 		book.author = author;
-		book.tittle = tittle;
-		return new ResponseEntity<BookEntity>(bookService.save(book), HttpStatus.OK);
+		book.tittle = title;
+		try {
+			return new ResponseEntity<BookEntity>(bookService.save(book), HttpStatus.CREATED);
+		} catch (Exception e) {
+			logger.error("Error while saving new book with title: " + title + " and author: " + author);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@RequestMapping("/all")
 	public ResponseEntity<?> findAll() {
+		logger.info("Getting all books");
 		return new ResponseEntity<List<BookEntity>>(bookService.findAll(), HttpStatus.OK);
 	}
 
 	@RequestMapping("/all/del")
-	public String deleteAll() {
-		bookService.deleteAll();
-		return "all books deleted";
+	public ResponseEntity<?> deleteAll() {
+		logger.info("Delete all books");
+		try {
+			bookService.deleteAll();
+		} catch (Exception e) {
+			logger.error("Error while deleting all books");
+			new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@RequestMapping("/del/{id}")
-	public String deleteById(@PathVariable("id") Long id) {
-		BookEntity entity = bookService.deleteById(id);
-		if (!entity.equals(null)) {
-			return "book with id=" + entity.getId() + "deleted";
+	public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
+		logger.info("Delete book with id= " + id);
+		BookEntity book;
+		try {
+			book = bookService.deleteById(id);
+		} catch (Exception e) {
+			logger.error("book with id= " + id + " is not exist");
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		return "book with id=" + id + " is not exist";
+		return new ResponseEntity<>(book, HttpStatus.OK);
 	}
 
 	@RequestMapping("/board/{bookId}")
@@ -56,7 +76,12 @@ public class BookController {
 
 	@RequestMapping("findT/{title}")
 	public ResponseEntity<?> findByTitle(@PathVariable("title") String title) {
+		logger.info("Getting books with title: " + title);
 		List<BookEntity> rezList = bookService.findByTittle(title);
+		if (rezList.isEmpty()) {
+			logger.info("Books with the title: " + title + " not found");
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 		return new ResponseEntity<List<BookEntity>>(rezList, HttpStatus.OK);
 	}
 }
--- src/by/intexsoft/sjt/controller/UserController.java
+++ src/by/intexsoft/sjt/controller/UserController.java
@@ -2,6 +2,7 @@ package by.intexsoft.sjt.controller;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,23 +12,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import by.intexsoft.sjt.entity.UserEntity;
 import by.intexsoft.sjt.service.UserService;
+import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+	private static Logger logger = (Logger) LoggerFactory.getLogger(UserController.class.getName());
+
 	@Autowired
 	UserService service;
 
 	@RequestMapping("/add/{first_name}")
 	public ResponseEntity<?> addUser(@PathVariable("first_name") String first_name) {
+		logger.info("Creation of a new user with the first name: " + first_name);
 		UserEntity user = new UserEntity();
 		user.name = first_name;
-		return new ResponseEntity<UserEntity>(service.save(user), HttpStatus.OK);
+		try {
+			return new ResponseEntity<UserEntity>(service.save(user), HttpStatus.CREATED);
+		} catch (Exception e) {
+			logger.error("Error while saving new user with first name: " + first_name);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@RequestMapping("/all")
 	public ResponseEntity<?> findAll() {
+		logger.info("Getting all users");
 		return new ResponseEntity<List<UserEntity>>(service.findAll(), HttpStatus.OK);
 	}
 }
