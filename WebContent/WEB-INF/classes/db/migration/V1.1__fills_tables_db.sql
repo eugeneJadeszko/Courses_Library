7c53cba8b078ca000ac05e631508f0120854833a
eugeneJadeszko <zhenia04.94@mail.ru>
2017-08-27 20:39:35 +0300

    connect flyway

---
+++ WebContent/WEB-INF/classes/db/migration/V1.1__fills_tables_db.sql
@@ -0,0 +1,12 @@
+INSERT INTO board (board_number) VALUES (1);
+INSERT INTO board (board_number) VALUES (2);
+
+INSERT INTO users (first_name) VALUES ('Eugene');
+INSERT INTO users (first_name) VALUES ('Inna');
+
+INSERT INTO books (tittle, author, board_id) VALUES ('master & margo', 'Bulgakov', 1);
+INSERT INTO books (tittle, author, board_id) VALUES ('Java', 'Herberdt Shildt', 1);
+
+INSERT INTO book_user (book_id, user_id) VALUES (1, 1);
+INSERT INTO book_user (book_id, user_id) VALUES (1, 2);
+INSERT INTO book_user (book_id, user_id) VALUES (2, 1);
